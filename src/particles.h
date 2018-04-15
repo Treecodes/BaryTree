f29cafa8b57f47c50e403bb5bdc8c994f1e7954e
Leighton W. Wilson <leightonwilson@mac.com>
2018-04-15 01:36:02 -0400

    Adding in the particles header

---
+++ src/particles.h
@@ -0,0 +1,15 @@
+#ifndef H_PARTICLES_H
+#define H_PARTICLES_H
+
+/* declaration of struct with tag particles */
+struct particles 
+{
+        int num;
+        double *x;
+        double *y;
+        double *z;
+        double *q;
+        int *order;
+};
+
+#endif /* H_PARTICLES_H */
