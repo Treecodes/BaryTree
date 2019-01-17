70711a3b8bdc59cd3541372cb35571b9ce2b75c4
nathanvaughn <nathanvaughn@192.168.1.101>
2019-01-16 19:24:31 -0500

    Added tests: to Makefile.  Segfault at runtime though.

--- tests/testQuadratureWeights.c
+++ tests/testQuadratureWeights.c
@@ -22,16 +22,58 @@
 
 
 
+/* initializing array for Clenshaw-Curtis weights */
+
+
+
 int main()
 {
-	struct tnode *testNode = NULL;
-	testNode = malloc(sizeof(struct tnode));
+	printf("Entering main() in tests...\n\n");
 
 	// Set the global variables
 	torder = 4;
 	torderlim = torder+1;
 
-	// Set bounds for the cluster
+
+	// Generate the Global Variable unscaledQuadratureWeights.  Nodes use this to compute their scaled quadrature weights
+	double CCtheta, b;
+	double *unscaledQuadratureWeights;
+	int i,j;
+
+	make_vector(unscaledQuadratureWeights, torderlim);
+
+	for ( i = 0; i < torderlim; i++ ){
+		  CCtheta = ( double ) ( i ) * M_PI / ( double ) ( torderlim - 1 );
+
+		  unscaledQuadratureWeights[i] = 1.0;
+
+		  for ( j = 1; j <= ( torderlim - 1 ) / 2; j++ )
+		  {
+			if ( 2 * j == ( torderlim - 1 ) ){
+			  b = 1.0;
+			}
+
+			else{
+			  b = 2.0;
+			}
+
+			unscaledQuadratureWeights[i] = unscaledQuadratureWeights[i] - b * cos ( 2.0 * ( double ) ( j ) * CCtheta )/ ( double ) ( 4 * j * j - 1 );
+		  }
+		}
+
+	unscaledQuadratureWeights[0] = unscaledQuadratureWeights[0] / ( double ) ( torderlim - 1 );
+	for ( i = 1; i < torderlim - 1; i++ ){
+		unscaledQuadratureWeights[i] = 2.0 * unscaledQuadratureWeights[i] / ( double ) ( torderlim - 1 );
+	}
+	unscaledQuadratureWeights[torderlim-1] = unscaledQuadratureWeights[torderlim-1] / ( double ) ( torderlim - 1 );
+
+
+	struct tnode *testNode = NULL;
+	testNode = malloc(sizeof(struct tnode));
+
+
+
+	// Setup the test node
 	double high=1.0;
 	double low=-1.0;
 	double mid = (high+low)/2.0;
@@ -53,7 +95,7 @@ int main()
 	pc_comp_weights(testNode)
 
 
-	for (int i=0; i<torderlim; i++){
+	for (i=0; i<torderlim; i++){
 		printf("x, y, z weights: %2.5f, %2.5f, %2.5f \n", testNode->wx[i], testNode->wy[i], testNode->wz[i] );
 	}
 
