8c52e3d8da9e6007e9463b03bd67dad0b8bc3e64
nathanvaughn <nathanvaughn@2379591021.vpn.umich.net>
2019-04-26 15:36:05 -0400

    tested 3.7 million point test case.

--- src/fluxRunDirectSum.sh
+++ src/fluxRunDirectSum.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#export OMP_NUM_THREADS=1
 
-#../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S21952.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T21952.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st21952_coulomb.bin out.tsv 21952 21952 0.0 0
-#../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st79576_coulomb.bin out.tsv 79576 79576 0.0 0
-#../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S348488.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T348488.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st348488_coulomb.bin out.tsv 348488 348488 0.0 0
+#../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S21952.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T21952.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st21952_coulomb.bin /home/njvaughn/MICDE_Data_2019/cpu_treecode/ds.tsv 21952 21952 0.0 0
+#../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st79576_coulomb.bin /home/njvaughn/MICDE_Data_2019/cpu_treecode/ds.tsv 79576 79576 0.0 0
+#../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S348488.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T348488.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st348488_coulomb.bin /home/njvaughn/MICDE_Data_2019/cpu_treecode/ds.tsv 348488 348488 0.0 0
+#../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_coulomb.bin /home/njvaughn/MICDE_Data_2019/cpu_treecode/ds.tsv 636608 636608 0.0 0
+ 
+ 
+#../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_coulomb_openACC.bin out.tsv 636608 636608 0.0 0
 
 
-../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_coulomb_openACC.bin out.tsv 636608 636608 0.0 0
-../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_yukawa_0p5_openACC.bin out.tsv 636608 636608 0.5 1
-
-
-nvidia-smi
-export PGI_ACC_TIME=0
+../bin_noAcc/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S3719492.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T3719492.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st3719492_coulomb_titan.bin out.tsv 3719492 3719492 0.0 0
+ 
+  
+#nvidia-smi
+#export PGI_ACC_TIME=0
 
 #../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S21952.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T21952.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st21952_coulomb_SS_0p5_openACC.bin out.tsv 21952 21952 0.5 2
 #../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st79576_coulomb_SS_0p5_openACC.bin out.tsv 79576 79576 0.5 2
@@ -30,7 +32,7 @@ export PGI_ACC_TIME=0
 #../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T79576.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st79576_coulomb_openACC.bin out.tsv 79576 79576 0.0 0
 #../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S348488.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T348488.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st348488_coulomb_openACC.bin out.tsv 348488 348488 0.0 0
 #../bin/direct.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_coulomb_openACC.bin out.tsv 636608 636608 0.0 0
-
+ 
 
 
 
--- src/fluxRunTreecode.sh
+++ src/fluxRunTreecode.sh
@@ -1,5 +1,139 @@
 #!/bin/bash
 
+#pgititan
+#nvidia-smi
+#export PGI_ACC_TIME=0
 
-#../bin/tree.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T636608.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_coulomb_openACC.bin out.csv        636608 636608 0.8 9 1 8000 0.0 0 1 0 0 8000
-../bin/tree.exe   /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S3719492.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T3719492.bin /scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_coulomb_openACC.bin out.csv        3719492 3719492 0.8 9 1 8000 0.0 0 1 0 0 8000
+
+
+
+TREETYPE=1
+
+SFLAG=1
+PFLAG=0
+DFLAG=0
+
+OUTFILE=/home/njvaughn/synchronizedDataFiles/MICDE_Data_2019/gpu_treecode/tc5.csv
+
+KAPPA=0.0
+POTENTIALTYPE=0 
+
+
+ 
+SOURCES=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S21952.bin
+TARGETS=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T21952.bin
+NUMSOURCES=21952
+NUMTARGETS=21952
+DIRECTSUM=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st21952_coulomb_titan.bin
+
+for BATCHSIZE in 1000 
+do
+	for MAXPARNODE in 1000
+	  do
+		for ORDER in 7
+		  do 
+		     for THETA in 0.9
+		     	do
+		     		../bin/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $SFLAG $PFLAG $DFLAG $BATCHSIZE
+		     done
+		 done
+	done
+done
+
+
+SOURCES=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S79576.bin
+TARGETS=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T79576.bin
+NUMSOURCES=79576
+NUMTARGETS=79576
+DIRECTSUM=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st79576_coulomb_titan.bin
+
+for BATCHSIZE in 2000 
+do
+	for MAXPARNODE in 2000
+	  do
+		for ORDER in 7
+		  do 
+		     for THETA in 0.9
+		     	do
+		     		../bin/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $SFLAG $PFLAG $DFLAG $BATCHSIZE
+		     done
+		 done
+	done
+done
+
+SOURCES=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S348488.bin
+TARGETS=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T348488.bin
+NUMSOURCES=348488
+NUMTARGETS=348488
+DIRECTSUM=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st348488_coulomb_titan.bin
+
+for BATCHSIZE in 2000 
+do
+	for MAXPARNODE in 4000
+	  do
+		for ORDER in 7
+		  do 
+		     for THETA in 0.9
+		     	do
+		     		../bin/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $SFLAG $PFLAG $DFLAG $BATCHSIZE
+		     done
+		 done
+	done
+done
+
+
+SOURCES=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S636608.bin
+TARGETS=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T636608.bin
+NUMSOURCES=636608
+NUMTARGETS=636608
+DIRECTSUM=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st636608_coulomb_titan.bin
+for BATCHSIZE in 8000 
+do
+	for MAXPARNODE in 8000
+	  do
+		for ORDER in 7
+		  do 
+		     for THETA in 0.9
+		     	do
+		     		../bin/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $SFLAG $PFLAG $DFLAG $BATCHSIZE
+		     done
+		 done
+	done
+done
+
+
+
+
+
+OUTFILE=/home/njvaughn/synchronizedDataFiles/MICDE_Data_2019/cpu_treecode/tc.csv
+
+
+SOURCES=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/S3719492.bin
+TARGETS=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/T3719492.bin
+NUMSOURCES=3719492
+NUMTARGETS=3719492
+DIRECTSUM=/scratch/krasny_fluxg/njvaughn/examplesOxygenAtom/ex_st3719492_coulomb_titan.bin
+
+for BATCHSIZE in 8000 
+do
+	for MAXPARNODE in 8000
+	  do
+		for ORDER in 7
+		  do 
+		     for THETA in 0.9
+		     	do
+		     		../bin_noAcc/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $SFLAG $PFLAG $DFLAG $BATCHSIZE
+		     done
+		 done
+	done
+done
+
+ 
+
+
+
+
+
+ 
+
+
