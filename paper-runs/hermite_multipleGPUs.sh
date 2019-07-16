b359cd28215f0878772ef57110835877b3d8296b
Nathanial Vaughn <njvaughn@comet-ln3.sdsc.edu>
2019-07-15 20:14:02 -0700

    comet commit

--- paper-runs/hermite_multipleGPUs.sh
+++ paper-runs/hermite_multipleGPUs.sh
@@ -6,7 +6,7 @@ DFLAG=0
 
 #N=821000
 #N=2365328
-N=10000000
+N=1000000
 #1328096
 
 
@@ -17,59 +17,41 @@ N=10000000
 #OUTFILE=/home/njvaughn/synchronizedDataFiles/KITCpaperData/hermiteTesting/coulomb/bughunt.csv
 
 #OUTFILE=/home/njvaughn/synchronizedDataFiles/KITCpaperData/parallelGPU/TitanV_hermite_parallel_$N.csv 
-OUTFILE=/home/njvaughn/synchronizedDataFiles/KITCpaperData/gpu_vs_cpu/schedule_testing_$N.csv 
+OUTFILE=/home/njvaughn/synchronizedDataFiles/KITCpaperData/comet/testing_$N.csv 
 
 
-SOURCES=/scratch/krasny_fluxg/njvaughn/random/S$N.bin    
-TARGETS=/scratch/krasny_fluxg/njvaughn/random/T$N.bin
+SOURCES=/oasis/scratch/comet/njvaughn/temp_project/random/S$N.bin    
+TARGETS=/oasis/scratch/comet/njvaughn/temp_project/random/T$N.bin
 #SOURCES=/scratch/krasny_fluxg/njvaughn/examplesBenzene/S$N.bin
 #TARGETS=/scratch/krasny_fluxg/njvaughn/examplesBenzene/T$N.bin
 NUMSOURCES=$N
 NUMTARGETS=$N
 #DIRECTSUM=/scratch/krasny_fluxg/njvaughn/random/ex_st_coulombSS_$N.bin  
 #DIRECTSUM=/scratch/krasny_fluxg/njvaughn/random/ex_st_yukawa_$N.bin  
-DIRECTSUM=/scratch/krasny_fluxg/njvaughn/random/ex_st_coulomb_$N.bin  
+DIRECTSUM=/oasis/scratch/comet/njvaughn/temp_project/random/ex_st_coulomb_$N.bin  
 #DS_CSV=/home/njvaughn/synchronizedDataFiles/KITCpaperData/hermiteTesting/coulomb/TitanV_directSum_GPU_parallelized.csv
-DS_CSV=/home/njvaughn/synchronizedDataFiles/KITCpaperData/gpu_vs_cpu/directSum_TitanV_Coulomb.csv 
+DS_CSV=/home/njvaughn/synchronizedDataFiles/KITCpaperData/comet/directSum_Coulomb.csv 
 
 
 NUMDEVICES=1
 KAPPA=0.0
 POTENTIALTYPE=0
 #../bin/direct.exe   $SOURCES $TARGETS $DIRECTSUM $DS_CSV $N $N $KAPPA $POTENTIALTYPE $NUMDEVICES  
-POTENTIALTYPE=4
-
-export OMP_SCHEDULE=static
-for ORDER in 5
-do
-	for THETA in 0.7
-	  do    
-		for BATCHSIZE in 5000
-		  do       
-		     for MAXPARNODE in 5000
-		     	do
-		     	for NUMDEVICES in 2     
-		     	do
-		     		../bin/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $PFLAG $SFLAG $DFLAG $BATCHSIZE $NUMDEVICES
-		     	done
-		     done 
-		 done
-	done
-done 
-
+POTENTIALTYPE=0
 
 export OMP_SCHEDULE=guided
-for ORDER in 5
+for ORDER in 8
 do
-	for THETA in 0.7
+	for THETA in 0.9
 	  do    
 		for BATCHSIZE in 5000
 		  do       
 		     for MAXPARNODE in 5000
 		     	do
-		     	for NUMDEVICES in 2     
+		     	for NUMDEVICES in 1 2 3 4     
 		     	do
-		     		../bin/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $PFLAG $SFLAG $DFLAG $BATCHSIZE $NUMDEVICES
+			../bin/direct.exe   $SOURCES $TARGETS $DIRECTSUM $DS_CSV $N $N $KAPPA $POTENTIALTYPE $NUMDEVICES
+		     		#../bin/tree.exe   $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER $TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $PFLAG $SFLAG $DFLAG $BATCHSIZE $NUMDEVICES
 		     	done
 		     done 
 		 done
--- paper-runs/random-data-examples/pointGenerator.py
+++ paper-runs/random-data-examples/pointGenerator.py
@@ -8,8 +8,8 @@ n=1
 N = int(sys.argv[n]); n+=1
 
 
-sourcesTXT = '/scratch/krasny_fluxg/njvaughn/random/S%ipy.txt' %N
-targetsTXT = '/scratch/krasny_fluxg/njvaughn/random/T%ipy.txt' %N
+sourcesTXT = '/oasis/scratch/comet/njvaughn/temp_project/random/S%ipy.txt' %N
+targetsTXT = '/oasis/scratch/comet/njvaughn/temp_project/random/T%ipy.txt' %N
 
 
 
