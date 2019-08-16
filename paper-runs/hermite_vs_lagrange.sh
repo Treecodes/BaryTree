c31f1740b91da4296b0ef870ac31a99442b6137f
nathanvaughn <nathanvaughn@2379590136.vpn.umich.net>
2019-08-16 10:25:36 -0400

    Debugging interaction lists.  Slow and incorrect on GPU.

--- paper-runs/hermite_vs_lagrange.sh
+++ paper-runs/hermite_vs_lagrange.sh
@@ -5,8 +5,8 @@ PFLAG=0
 DFLAG=0
 
 N=1000000
-BATCHSIZE=5000
-MAXPARNODE=5000
+BATCHSIZE=2000
+MAXPARNODE=2000
 SOURCES=/scratch/krasny_fluxg/njvaughn/random/S$N.bin    
 TARGETS=/scratch/krasny_fluxg/njvaughn/random/T$N.bin
 NUMSOURCES=$N
@@ -22,15 +22,16 @@ DIRECTSUM=/scratch/krasny_fluxg/njvaughn/random/ex_st_coulomb_$N.bin
 #OUTFILE=/home/njvaughn/synchronizedDataFiles/KITCpaperData/treecodeVersusDirectSum/coulomb_hermite.csv 
 OUTFILE=/home/njvaughn/testing/coulomb_hermite.csv 
 
+direct-gpu $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $KAPPA $POTENTIALTYPE $NUMDEVICES $NUMTHREADS
 for ORDER in 7
   do   
      for THETA in 0.8    
      	do
      	echo $THETA
      	#direct-gpu  $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $KAPPA $POTENTIALTYPE $NUMDEVICES $NUMTHREADS
- 		#tree-gpu   	$SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER \
- 		#			$TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $PFLAG $SFLAG $DFLAG $BATCHSIZE \
- 		#			$NUMDEVICES $NUMTHREADS
+ 		tree-gpu   	$SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER \
+ 					$TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $PFLAG $SFLAG $DFLAG $BATCHSIZE \
+ 					$NUMDEVICES $NUMTHREADS
      done
  done
 
@@ -42,15 +43,15 @@ KAPPA=0.5
 POTENTIALTYPE=5 
 DIRECTSUM=/scratch/krasny_fluxg/njvaughn/random/ex_st_yukawa_$N.bin
 OUTFILE=/home/njvaughn/synchronizedDataFiles/KITCpaperData/treecodeVersusDirectSum/yukawa_hermite.csv 
-for ORDER in 5
-  do   
-     for THETA in 0.7    
-     	do
-     	direct-gpu  $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $KAPPA $POTENTIALTYPE $NUMDEVICES $NUMTHREADS
- 		tree-gpu   	$SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER \
- 					$TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $PFLAG $SFLAG $DFLAG $BATCHSIZE \
- 					$NUMDEVICES $NUMTHREADS
-     done
- done
+#for ORDER in 5
+#  do   
+#     for THETA in 0.7    
+#     	do
+#     	direct-gpu  $SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $KAPPA $POTENTIALTYPE $NUMDEVICES $NUMTHREADS
+# 		tree-gpu   	$SOURCES $TARGETS $DIRECTSUM $OUTFILE $NUMSOURCES $NUMTARGETS $THETA $ORDER \
+# 					$TREETYPE $MAXPARNODE $KAPPA $POTENTIALTYPE $PFLAG $SFLAG $DFLAG $BATCHSIZE \
+# 					$NUMDEVICES $NUMTHREADS
+#     done
+# done
  
  
--- src/tree-pc.c
+++ src/tree-pc.c
@@ -304,37 +304,37 @@ void pc_partition_8(double *x, double *y, double *z, double *q, double *w, doubl
 
 
 
-void pc_create_tree_array(struct tnode *p, struct tnode_array *tree_array)
-{
-    //    printf("Entering pc_create_tree_array.\n");
-    int i;
-
-    /*midpoint coordinates, RADIUS and SQRADIUS*/
-    tree_array->x_mid[p->node_index] = p->x_mid;
-    tree_array->y_mid[p->node_index] = p->y_mid;
-    tree_array->z_mid[p->node_index] = p->z_mid;
-
-    tree_array->x_min[p->node_index] = p->x_min;
-    tree_array->y_min[p->node_index] = p->y_min;
-    tree_array->z_min[p->node_index] = p->z_min;
-
-    tree_array->x_max[p->node_index] = p->x_max;
-    tree_array->y_max[p->node_index] = p->y_max;
-    tree_array->z_max[p->node_index] = p->z_max;
-
-    tree_array->ibeg[p->node_index] = p->ibeg;
-    tree_array->iend[p->node_index] = p->iend;
-    tree_array->numpar[p->node_index] = p->numpar;
-    tree_array->level[p->node_index] = p->level;
-    tree_array->radius[p->node_index] = p->radius;
-
-    for (i = 0; i < p->num_children; i++) {
-        pc_create_tree_array(p->child[i], tree_array);
-    }
-
-    return;
-
-} /* END of function create_tree_n0 */
+//void pc_create_tree_array(struct tnode *p, struct tnode_array *tree_array)
+//{
+//    //    printf("Entering pc_create_tree_array.\n");
+//    int i;
+//
+//    /*midpoint coordinates, RADIUS and SQRADIUS*/
+//    tree_array->x_mid[p->node_index] = p->x_mid;
+//    tree_array->y_mid[p->node_index] = p->y_mid;
+//    tree_array->z_mid[p->node_index] = p->z_mid;
+//
+//    tree_array->x_min[p->node_index] = p->x_min;
+//    tree_array->y_min[p->node_index] = p->y_min;
+//    tree_array->z_min[p->node_index] = p->z_min;
+//
+//    tree_array->x_max[p->node_index] = p->x_max;
+//    tree_array->y_max[p->node_index] = p->y_max;
+//    tree_array->z_max[p->node_index] = p->z_max;
+//
+//    tree_array->ibeg[p->node_index] = p->ibeg;
+//    tree_array->iend[p->node_index] = p->iend;
+//    tree_array->numpar[p->node_index] = p->numpar;
+//    tree_array->level[p->node_index] = p->level;
+//    tree_array->radius[p->node_index] = p->radius;
+//
+//    for (i = 0; i < p->num_children; i++) {
+//        pc_create_tree_array(p->child[i], tree_array);
+//    }
+//
+//    return;
+//
+//} /* END of function create_tree_n0 */
 
 
 
