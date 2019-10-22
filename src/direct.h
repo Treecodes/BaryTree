f434235c03ba9c643cb06a3dd38a7206106b9753
Leighton W. Wilson <leightonwilson@mac.com>
2019-10-22 00:40:42 -0400

    modifying example

---
+++ src/direct.h
@@ -0,0 +1,9 @@
+#ifndef H_DIRECT_H
+#define H_DIRECT_H
+
+void direct_eng(double *xS, double *yS, double *zS, double *qS, double *wS,
+                double *xT, double *yT, double *zT, double *qT,
+                int numparsS, int numparsT, double *denergy, double *dpeng,
+                int pot_type, double kappa);
+
+#endif /* H_DIRECT_H */
