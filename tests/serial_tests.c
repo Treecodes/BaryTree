7bd43181fc8f7f9b6d7fd65223f867356cafa4e4
nathanvaughn <nathanvaughn@2379589790.vpn.umich.net>
2019-11-17 14:44:21 -0500

    Added test to verify treecode parameters improve accuracy

--- tests/serial_tests.c
+++ tests/serial_tests.c
@@ -861,6 +861,464 @@ static char * test_treecode_on_1_target_10000_sources() {
 }
 
 
+static char * test_treecode_parameters_on_1_target_10000_sources() {
+
+    int N=10000;
+    int verbose=0;
+
+    struct particles *sources = NULL;
+    struct particles *targets = NULL;
+    int *particleOrder = NULL;
+    double *potential1 = NULL, *potential2 = NULL, *potential3 = NULL, *potential_direct = NULL;
+    double potential_engy = 0;
+    double potential_engy_direct = 0;
+
+    sources = malloc(sizeof(struct particles));
+    targets = malloc(sizeof(struct particles));
+    potential1 = malloc(sizeof(double) * N);
+    potential2 = malloc(sizeof(double) * N);
+    potential3 = malloc(sizeof(double) * N);
+    potential_direct = malloc(sizeof(double) * N);
+    particleOrder = malloc(sizeof(int) * N);
+
+    targets->num = 1; //single target
+    targets->x = malloc(targets->num*sizeof(double));
+    targets->y = malloc(targets->num*sizeof(double));
+    targets->z = malloc(targets->num*sizeof(double));
+    targets->q = malloc(targets->num*sizeof(double));
+    targets->order = malloc(targets->num*sizeof(int));
+
+
+    sources->num = N; // 10,000 sources
+    sources->x = malloc(sources->num*sizeof(double));
+    sources->y = malloc(sources->num*sizeof(double));
+    sources->z = malloc(sources->num*sizeof(double));
+    sources->q = malloc(sources->num*sizeof(double));
+    sources->w = malloc(sources->num*sizeof(double));
+    sources->order = malloc(sources->num*sizeof(int));
+
+
+    for (int i=0; i<targets->num; i++){
+        // single target at origin
+        targets->x[i]=0.0;
+        targets->y[i]=0.0;
+        targets->z[i]=0.0;
+        targets->q[i]=1.0;
+        targets->order[i] = i;
+        }
+
+    srand(1);
+    for (int i=0; i<sources->num; i++){
+        // 10,000 randomly distributed sources in the [-1,1] box
+        sources->x[i]=((double)rand()/(double)(RAND_MAX)) * 2. - 1.;
+        sources->y[i]=((double)rand()/(double)(RAND_MAX)) * 2. - 1.;
+        sources->z[i]=((double)rand()/(double)(RAND_MAX)) * 2. - 1.;
+        sources->q[i]=((double)rand()/(double)(RAND_MAX)) * 2. - 1.;
+        sources->w[i]=((double)rand()/(double)(RAND_MAX));
+        sources->order[i] = i;
+        }
+
+
+    int max_per_leaf=100;
+    int max_per_batch=100;
+    double time_tree[9];
+
+    char *kernelName, *singularityHandling, *approximationName;
+
+
+    int tree_type=1; // particle-cluster
+    double kappa=0.5;
+
+    // 3 parameter sets.  Set 2 increases order, set 3 reduces MAC.  Both should be more accurate than set 1.
+    int order1=4;
+    double theta1=0.8;
+
+    int order2=5;
+    double theta2=0.8;
+
+    int order3=4;
+    double theta3=0.5;
+
+    /***********************************************/
+    /******************* Test 1 ********************/
+    /***********************************************/
+    /********** lagrange-coulomb-skipping **********/
+    /***********************************************/
+    approximationName="lagrange";
+    kernelName="coulomb";
+    singularityHandling="skipping";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-coulomb-skipping", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-coulomb-skipping", \
+                err3 < err1);
+    }
+
+
+
+    /***********************************************/
+    /******************* Test 2 ********************/
+    /***********************************************/
+    /********* lagrange-coulomb-subtraction ********/
+    /***********************************************/
+    approximationName="lagrange";
+    kernelName="coulomb";
+    singularityHandling="subtraction";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-coulomb-subtraction", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-coulomb-subtraction", \
+                err3 < err1);
+    }
+
+
+    /***********************************************/
+    /******************* Test 3 ********************/
+    /***********************************************/
+    /*********** lagrange-yukawa-skipping **********/
+    /***********************************************/
+    approximationName="lagrange";
+    kernelName="yukawa";
+    singularityHandling="skipping";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-yukawa-skipping", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-yukawa-skipping", \
+                err3 < err1);
+    }
+
+    /***********************************************/
+    /******************* Test 4 ********************/
+    /***********************************************/
+    /********* lagrange-yukawa-subtraction *********/
+    /***********************************************/
+    approximationName="lagrange";
+    kernelName="yukawa";
+    singularityHandling="subtraction";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-yukawa-subtraction", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: lagrange-yukawa-subtraction", \
+                err3 < err1);
+    }
+
+    /***********************************************/
+    /******************* Test 5 ********************/
+    /***********************************************/
+    /********* hermite-coulomb-skipping ************/
+    /***********************************************/
+    approximationName="hermite";
+    kernelName="coulomb";
+    singularityHandling="skipping";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-coulomb-skipping", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-coulomb-skipping", \
+                err3 < err1);
+    }
+
+
+    /***********************************************/
+    /******************* Test 6 ********************/
+    /***********************************************/
+    /******** hermite-coulomb-subtraction **********/
+    /***********************************************/
+    approximationName="hermite";
+    kernelName="coulomb";
+    singularityHandling="subtraction";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-coulomb-subtraction", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-coulomb-subtraction", \
+                err3 < err1);
+    }
+
+    /***********************************************/
+    /******************* Test 7 ********************/
+    /***********************************************/
+    /********** hermite-yukawa-skipping ************/
+    /***********************************************/
+    approximationName="hermite";
+    kernelName="yukawa";
+    singularityHandling="skipping";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-yukawa-skipping", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-yukawa-skipping", \
+                err3 < err1);
+    }
+
+    /***********************************************/
+    /******************* Test 8 ********************/
+    /***********************************************/
+    /********* hermite-yukawa-subtraction **********/
+    /***********************************************/
+    approximationName="hermite";
+    kernelName="yukawa";
+    singularityHandling="subtraction";
+    for (int i=0; i<targets->num; i++){
+        potential1[i]=0.0;
+        potential2[i]=0.0;
+        potential3[i]=0.0;
+        potential_direct[i]=0.0;
+    }
+
+    directdriver(sources, targets, kernelName, kappa, singularityHandling, approximationName,
+                 potential_direct, time_tree);
+
+    treedriver(sources, targets, order1, theta1, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential1, time_tree);
+
+    treedriver(sources, targets, order2, theta2, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential2, time_tree);
+
+    treedriver(sources, targets, order3, theta3, max_per_leaf, max_per_batch,
+               kernelName, kappa, singularityHandling, approximationName, tree_type,
+               potential3, time_tree);
+
+    for (int i=0; i<targets->num; i++){
+        double err1 = fabs(potential1[i] - potential_direct[i]);
+        double err2 = fabs(potential2[i] - potential_direct[i]);
+        double err3 = fabs(potential3[i] - potential_direct[i]);
+
+        if (verbose>0) printf("err1 = %1.4e\n", err1);
+        if (verbose>0) printf("err2 = %1.4e\n", err2);
+        if (verbose>0) printf("err3 = %1.4e\n", err3);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-yukawa-subtraction", \
+                err2 < err1);
+        mu_assert("TEST FAILED: increasing order didn't improve accuracy for: hermite-yukawa-subtraction", \
+                err3 < err1);
+    }
+
+    free(sources->x);
+    free(sources->y);
+    free(sources->z);
+    free(sources->q);
+    free(sources->w);
+    free(sources->order);
+    free(sources);
+
+    free(targets->x);
+    free(targets->y);
+    free(targets->z);
+    free(targets->q);
+    free(targets->order);
+    free(targets);
+
+    free(potential1);
+    free(potential2);
+    free(potential3);
+    free(potential_direct);
+    return 0;
+}
+
+
+
 // Run all the tests
 static char *all_tests() {
     mu_run_test(test_direct_sum_on_10_particles);
@@ -869,6 +1327,8 @@ static char *all_tests() {
     printf("Completed test_treecode_on_100_particles().\n");
     mu_run_test(test_treecode_on_1_target_10000_sources);
     printf("Completed test_treecode_on_1_target_10000_sources().\n");
+    mu_run_test(test_treecode_parameters_on_1_target_10000_sources);
+    printf("Completed test_treecode_parameters_on_1_target_10000_sources().\n");
     return 0;
 }
 
