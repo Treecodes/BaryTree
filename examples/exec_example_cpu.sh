d84fd5745bed7bc518c841fe48b16afe059420b1
Leighton W. Wilson <leightonwilson@mac.com>
2020-03-03 12:35:13 -0500

    Delete exec_example_cpu.sh

--- examples/exec_example_cpu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 make_points.py sources sources_1E4.bin 10000
-python3 make_points.py targets targets_1E4.bin 10000
-direct-cpu sources_1E4.bin targets_1E4.bin direct_benchmark.bin direct_summary.csv 10000 10000 0.0 0 1
-tree-cpu sources_1E4.bin targets_1E4.bin direct_benchmark.bin tree_summary.csv 10000 10000 0.5 5 500 5 0 0.0 1
