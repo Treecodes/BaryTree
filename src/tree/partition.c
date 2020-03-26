569182cdfc2c2462a552f5f68db38b40f3da0acb
Leighton W. Wilson <leightonwilson@mac.com>
2020-03-26 14:33:54 -0400

    Adding leaf size checking to cp partition

--- src/tree/partition.c
+++ src/tree/partition.c
@@ -312,9 +312,15 @@ void cp_partition_8(double *x, double *y, double *z, double *q, int *orderarr, d
         if (yl < xl || yl < zl) divide_y = 0;
         if (zl < xl || zl < yl) divide_z = 0;
         
-        if (divide_x + divide_y + divide_z >= 2) {
+        if (divide_x + divide_y + divide_z == 3) {
             divide_x = 0;
             divide_y = 0;
+        }
+        
+        if (divide_x + divide_y + divide_z == 2) {
+            if (divide_x == 0) divide_y = 0;
+            if (divide_y == 0) divide_z = 0;
+            if (divide_z == 0) divide_x = 0;
         }
     }
 
