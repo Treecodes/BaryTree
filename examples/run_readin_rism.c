47a2ded29df70574593d9f23268d3824151fc7eb
Leighton W. Wilson <leightonwilson@mac.com>
2020-06-17 17:26:15 -0400

    Just an example for running rism

---
+++ examples/run_readin_rism.c
@@ -0,0 +1,265 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <float.h>
+#include <mpi.h>
+
+#include "../src/utilities/tools.h"
+#include "../src/utilities/timers.h"
+
+#include "../src/particles/struct_particles.h"
+#include "../src/run_params/struct_run_params.h"
+#include "../src/run_params/run_params.h"
+
+#include "../src/drivers/treedriver.h"
+#include "../src/drivers/directdriver.h"
+
+#include "support_fns.h"
+
+
+int main(int argc, char **argv)
+{
+    /* MPI initialization */
+    int rank, numProcs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    if (rank == 0) printf("[random cube example] Beginning random cube example with %d ranks.\n", numProcs);
+
+    /* run parameters */
+    int N, run_direct, slice;
+    double xyz_limits[6];
+    int grid_dim[3];
+    double grid_dd[3];
+    FILE *fp_pqr;
+    char file_pqr[256];
+    
+    struct RunParams *run_params = NULL;
+    
+    FILE *fp = fopen(argv[1], "r");
+    Params_Parse_Readin(fp, &run_params, &N, file_pqr, &run_direct, &slice, xyz_limits, grid_dim);
+
+    grid_dd[0] = (xyz_limits[1] - xyz_limits[0]) / (grid_dim[0] - 1);
+    grid_dd[1] = (xyz_limits[3] - xyz_limits[2]) / (grid_dim[1] - 1);
+    grid_dd[2] = (xyz_limits[5] - xyz_limits[4]) / (grid_dim[2] - 1);
+
+
+    /* data structures for BaryTree calculation and comparison */
+    struct Particles *sources = NULL;
+    struct Particles *targets = NULL;
+    struct Particles *targets_sample = NULL;
+    double *potential = NULL, *potential_direct = NULL;
+    
+    /* variables for collecting accuracy info */
+    double potential_engy = 0, potential_engy_glob = 0;
+    double potential_engy_direct = 0, potential_engy_direct_glob = 0;
+    double glob_inf_err = 0, glob_n2_err = 0, glob_relinf_err = 0, glob_reln2_err = 0;
+
+    /* variables for date-time calculation */
+    double time_run[4], time_tree[13], time_direct[4];
+    double time_run_glob[3][4], time_tree_glob[3][13], time_direct_glob[3][4];
+
+
+    /* Beginning total runtime timer */
+    START_TIMER(&time_run[3]);
+    
+    
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Setup
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    START_TIMER(&time_run[0]);
+
+
+    /* Setting up sources with MPI-allocated source arrays for RMA use */
+    
+    sources = malloc(sizeof(struct Particles));
+    sources->num = N;
+
+    MPI_Alloc_mem(sources->num * sizeof(double), MPI_INFO_NULL, &(sources->x));
+    MPI_Alloc_mem(sources->num * sizeof(double), MPI_INFO_NULL, &(sources->y));
+    MPI_Alloc_mem(sources->num * sizeof(double), MPI_INFO_NULL, &(sources->z));
+    MPI_Alloc_mem(sources->num * sizeof(double), MPI_INFO_NULL, &(sources->q));
+    MPI_Alloc_mem(sources->num * sizeof(double), MPI_INFO_NULL, &(sources->w));
+
+    char c[256], c1[120], c2[120], c3[120], c4[10], c5[10];
+    double a1, a2, a3, b1, b2;
+    int atom_ctr = 0;
+
+    FILE *points_fp = fopen(file_pqr, "r");
+
+    while (fgets(c, sizeof(c), points_fp)) {
+        sscanf(c, "%s %s %s %s %s %lf %lf %lf %lf %lf",
+               c1, c2, c3, c4, c5, &a1, &a2, &a3, &b1, &b2);
+
+        if (strncmp(c1, "ATOM", 4) == 0) {
+            sources->x[atom_ctr] = a1;
+            sources->y[atom_ctr] = a2;
+            sources->z[atom_ctr] = a3;
+            sources->q[atom_ctr] = b2;
+            sources->w[atom_ctr] = 1.0;
+            atom_ctr++;
+        }
+    }
+    fclose(points_fp);
+
+//    for (int i = 0; i < N; ++i) {
+//        fscanf(points_fp, "%lf %lf %lf %lf", &(sources->x[i]), &(sources->y[i]), &(sources->z[i]), &(sources->q[i]));
+//        sources->w[i] = 1.0;
+//    }
+
+    
+    /* Setting up targets */
+    
+    targets = malloc(sizeof(struct Particles));
+    targets->num = grid_dim[0] * grid_dim[1] * grid_dim[2];
+    
+    MPI_Alloc_mem(targets->num * sizeof(double), MPI_INFO_NULL, &(targets->x));
+    MPI_Alloc_mem(targets->num * sizeof(double), MPI_INFO_NULL, &(targets->y));
+    MPI_Alloc_mem(targets->num * sizeof(double), MPI_INFO_NULL, &(targets->z));
+    MPI_Alloc_mem(targets->num * sizeof(double), MPI_INFO_NULL, &(targets->q));
+
+    for (int k = 0; k < grid_dim[2]; ++k) {
+        double zz = xyz_limits[4] + k * grid_dd[2];
+        int grid_pos_k = k * grid_dim[1] * grid_dim[0];
+
+        for (int j = 0; j < grid_dim[1]; ++j) {
+            double yy = xyz_limits[2] + j * grid_dd[1];
+            int grid_pos_jk = grid_pos_k + j * grid_dim[0];
+
+            for (int i = 0; i < grid_dim[0]; ++i) {
+                double xx = xyz_limits[0] + i * grid_dd[0];
+
+                targets->x[grid_pos_jk + i] = xx;
+                targets->y[grid_pos_jk + i] = yy;
+                targets->z[grid_pos_jk + i] = zz;
+                targets->q[grid_pos_jk + i] = 1.0;
+            }
+        }
+    }
+               
+
+
+
+    if (rank == 0) printf("[random cube example] Setup has finished.\n");
+
+    /* Initializing direct and treedriver runs */
+
+    targets_sample = malloc(sizeof(struct Particles));
+
+    potential = malloc(sizeof(double) * N);
+    potential_direct = malloc(sizeof(double) * N);
+
+    memset(potential, 0, targets->num * sizeof(double));
+    memset(potential_direct, 0, targets->num * sizeof(double));
+
+
+#ifdef OPENACC_ENABLED
+    #pragma acc set device_num(rank) device_type(acc_device_nvidia)
+    #pragma acc init device_type(acc_device_nvidia)
+#endif
+
+    STOP_TIMER(&time_run[0]);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Running direct comparison
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    if (run_direct == 1) {
+
+        targets_sample->num = targets->num / slice;
+        targets_sample->x = malloc(targets_sample->num * sizeof(double));
+        targets_sample->y = malloc(targets_sample->num * sizeof(double));
+        targets_sample->z = malloc(targets_sample->num * sizeof(double));
+        targets_sample->q = malloc(targets_sample->num * sizeof(double));
+
+        for (int i = 0; i < targets_sample->num; i++) {
+            targets_sample->x[i] = targets->x[i*slice];
+            targets_sample->y[i] = targets->y[i*slice];
+            targets_sample->z[i] = targets->z[i*slice];
+            targets_sample->q[i] = targets->q[i*slice];
+        }
+
+        if (rank == 0) printf("[random cube example] Running direct comparison...\n");
+
+        START_TIMER(&time_run[1]);
+        directdriver(sources, targets_sample, run_params, potential_direct, time_direct);
+        STOP_TIMER(&time_run[1]);
+
+        free(targets_sample->x);
+        free(targets_sample->y);
+        free(targets_sample->z);
+        free(targets_sample->q);
+        free(targets_sample);
+
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Running treecode
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    if (rank == 0) printf("[random cube example] Running treedriver...\n");
+
+    START_TIMER(&time_run[2]);
+    treedriver(sources, targets, run_params, potential, time_tree);
+    STOP_TIMER(&time_run[2]);
+
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    /* Ending total runtime timer */
+    STOP_TIMER(&time_run[3]);
+
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Calculate results
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Timing_Calculate(time_run_glob, time_tree_glob, time_direct_glob,
+                     time_run, time_tree, time_direct);
+    Timing_Print(time_run_glob, time_tree_glob, time_direct_glob, run_direct, run_params);
+    
+    if (run_direct == 1) {
+        Accuracy_Calculate(&potential_engy_glob, &potential_engy_direct_glob,
+                           &glob_inf_err, &glob_relinf_err, &glob_n2_err, &glob_reln2_err,
+                           potential, potential_direct, targets->num, slice);
+        Accuracy_Print(potential_engy_glob, potential_engy_direct_glob,
+                           glob_inf_err, glob_relinf_err, glob_n2_err, glob_reln2_err, slice);
+    }
+    
+    CSV_Print(N, M, run_params, time_run_glob, time_tree_glob, time_direct_glob,
+              potential_engy_glob, potential_engy_direct_glob,
+              glob_inf_err, glob_relinf_err, glob_n2_err, glob_reln2_err);
+
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Cleanup
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    MPI_Free_mem(sources->x);
+    MPI_Free_mem(sources->y);
+    MPI_Free_mem(sources->z);
+    MPI_Free_mem(sources->q);
+    MPI_Free_mem(sources->w);
+    free(sources);
+
+    MPI_Free_mem(targets->x);
+    MPI_Free_mem(targets->y);
+    MPI_Free_mem(targets->z);
+    MPI_Free_mem(targets->q);
+    free(targets);
+
+    free(potential);
+    free(potential_direct);
+
+    RunParams_Free(&run_params);
+
+    MPI_Finalize();
+
+    return 0;
+}
--- examples/support_fns.c
+++ examples/support_fns.c
@@ -295,6 +295,260 @@ void Params_Parse(FILE *fp, struct RunParams **run_params, int *N, int *M, int *
 
 
 
+
+/*----------------------------------------------------------------------------*/
+void Params_Parse_Readin(FILE *fp, struct RunParams **run_params, int *N, char *file_pqr,
+                         int *run_direct, int *slice, double *xyz_limits, int *grid_dim)
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    /* BaryTree params */
+    int verbosity = 0;
+    int interp_order = 5; 
+    double theta = 0.5; 
+    int max_per_source_leaf = 500;
+    int max_per_target_leaf = 500; 
+    double size_check_factor = 1.0;
+    
+    char kernel_string[256]        = "COULOMB";
+    char singularity_string[256]   = "SKIPPING";
+    char approximation_string[256] = "LAGRANGE";
+    char compute_type_string[256]  = "PARTICLE_CLUSTER";
+    char run_direct_string[256]    = "OFF";
+
+    KERNEL kernel;
+    SINGULARITY singularity;
+    APPROXIMATION approximation;
+    COMPUTE_TYPE compute_type;
+
+    int num_kernel_params = 0;
+    double kernel_params[32];
+
+    /* random_cube_example params */
+    *N = 10000; 
+    *slice = 1;
+    
+    xyz_limits[0] = -1.;
+    xyz_limits[1] =  1.;
+    xyz_limits[2] = -1.;
+    xyz_limits[3] =  1.;
+    xyz_limits[4] = -1.;
+    xyz_limits[5] =  1.;
+
+    grid_dim[0] = 100;
+    grid_dim[1] = 100;
+    grid_dim[2] = 100;
+
+
+    char c[256], c1[256], c2[256];
+
+    while (fgets(c, 256, fp) != NULL) {
+        sscanf(c, "%s %s", c1, c2);
+    
+        /* Parameters for the RunParam struct */
+        if (strcmp(c1, "order") == 0) {
+            interp_order = atoi(c2);
+
+        } else if (strcmp(c1, "theta") == 0) {
+            theta = atof(c2);
+
+        } else if (strcmp(c1, "max_per_source_leaf") == 0) {
+            max_per_source_leaf = atoi(c2);
+
+        } else if (strcmp(c1, "max_per_target_leaf") == 0) {
+            max_per_target_leaf = atoi(c2);
+
+        } else if (strcmp(c1, "kernel_name") == 0) {
+            strcpy(kernel_string, c2);
+        
+        } else if (strcmp(c1, "kernel_params") == 0) {
+            char *k_param = strtok(c2, " ,");
+            while (k_param != NULL) {
+                kernel_params[num_kernel_params] = atof(k_param);
+                num_kernel_params++;
+                k_param = strtok(NULL, " ,");
+            }
+        
+        } else if (strcmp(c1, "singularity") == 0) {
+            strcpy(singularity_string, c2);
+        
+        } else if (strcmp(c1, "approximation") == 0) {
+            strcpy(approximation_string, c2);
+        
+        } else if (strcmp(c1, "compute_type") == 0) {
+            strcpy(compute_type_string, c2);
+        
+        } else if (strcmp(c1, "size_check") == 0) {
+            size_check_factor = atof(c2);
+        
+        } else if (strcmp(c1, "verbosity") == 0) {
+            verbosity = atoi(c2);
+
+        /* Other run parameters */
+        } else if (strcmp(c1, "num_sources") == 0) {
+            *N = atoi(c2);
+
+        } else if (strcmp(c1, "run_direct") == 0) {
+            strcpy(run_direct_string, c2);
+
+        } else if (strcmp(c1, "slice") == 0) {
+            *slice = atoi(c2);
+            
+        } else if (strcmp(c1, "file_pqr") == 0) {
+            strcpy(file_pqr, c2);
+
+        } else if (strcmp(c1, "xyz_limits") == 0) {
+            char *xyz_limits_string = strtok(c2, " ,");
+            int num_xyz_limits;
+            while (xyz_limits_string != NULL) {
+                xyz_limits[num_xyz_limits] = atof(xyz_limits_string);
+                num_xyz_limits++;
+                xyz_limits_string = strtok(NULL, " ,");
+            }
+
+        } else if (strcmp(c1, "grid_dim") == 0) {
+            char *grid_dim_string = strtok(c2, " ,");
+            int num_grid_dim;
+            while (grid_dim_string != NULL) {
+                grid_dim[num_grid_dim] = atoi(grid_dim_string);
+                num_grid_dim++;
+                grid_dim_string = strtok(NULL, " ,");
+            }
+
+        } else {
+            if (rank == 0) {
+                printf("[random cube example] ERROR! Undefined token \"%s\". Exiting.\n", c1);
+            }
+            exit(1);
+        }
+    }
+    
+
+    /* Validating tokens for RunParam struct */
+    if (strcasecmp(kernel_string, "COULOMB") == 0) {
+        kernel = COULOMB;
+
+    } else if (strcasecmp(kernel_string, "YUKAWA") == 0) {
+        kernel = YUKAWA;
+    
+    } else if ((strcasecmp(kernel_string, "REGULARIZED_COULOMB") == 0)
+            || (strcasecmp(kernel_string, "REGULARIZED-COULOMB") == 0)) {
+        kernel = REGULARIZED_COULOMB;
+    
+    } else if ((strcasecmp(kernel_string, "REGULARIZED_YUKAWA") == 0)
+            || (strcasecmp(kernel_string, "REGULARIZED-YUKAWA") == 0)) {
+        kernel = REGULARIZED_YUKAWA;
+    
+    } else if (strcasecmp(kernel_string, "ATAN") == 0) {
+        kernel = ATAN;
+
+    } else if (strcasecmp(kernel_string, "MQ") == 0) {
+        kernel = MQ;
+    
+    } else if (strcasecmp(kernel_string, "TCF") == 0) {
+        kernel = TCF;
+    
+    } else if (strcasecmp(kernel_string, "DCF") == 0) {
+        kernel = DCF;
+
+    } else if ((strcasecmp(kernel_string, "SIN_OVER_R") == 0)
+            || (strcasecmp(kernel_string, "SIN-OVER-R") == 0)) {
+        kernel = SIN_OVER_R;
+    
+    } else {
+        if (rank == 0) {
+            printf("[random cube example] ERROR! Undefined kernel token \"%s\". Exiting.\n",
+                   kernel_string);
+        }
+        exit(1);
+    }
+
+
+    if (strcasecmp(singularity_string, "SKIPPING") == 0) {
+        singularity = SKIPPING;
+
+    } else if (strcasecmp(singularity_string, "SUBTRACTION") == 0) {
+        singularity = SUBTRACTION;
+
+    } else {
+        if (rank == 0) {
+            printf("[random cube example] ERROR! Undefined singularity token \"%s\". Exiting.\n",
+                   singularity_string);
+        }
+        exit(1);
+    }
+
+
+    if (strcasecmp(approximation_string, "LAGRANGE") == 0) {
+        approximation = LAGRANGE;
+
+    } else if (strcasecmp(approximation_string, "HERMITE") == 0) {
+        approximation = HERMITE;
+    
+    } else {
+        if (rank == 0) {
+            printf("[random cube example] ERROR! Undefined approximation token \"%s\". Exiting.\n",
+                   approximation_string);
+        }
+        exit(1);
+    }
+
+
+    if ((strcasecmp(compute_type_string, "CLUSTER_PARTICLE") == 0)
+     || (strcasecmp(compute_type_string, "CLUSTER-PARTICLE") == 0)) {
+        compute_type = CLUSTER_PARTICLE;
+
+    } else if ((strcasecmp(compute_type_string, "PARTICLE_CLUSTER") == 0)
+            || (strcasecmp(compute_type_string, "PARTICLE-CLUSTER") == 0)) {
+        compute_type = PARTICLE_CLUSTER;
+
+    } else if ((strcasecmp(compute_type_string, "CLUSTER_CLUSTER") == 0)
+            || (strcasecmp(compute_type_string, "CLUSTER-CLUSTER") == 0)) {
+        compute_type = CLUSTER_CLUSTER;
+
+    } else {
+        if (rank == 0) {
+            printf("[random cube example] ERROR! Undefined compute_type token \"%s\". Exiting.\n",
+                   compute_type_string);
+        }
+        exit(1);
+    }
+    
+    
+    /* Validating other tokens */
+    if ((strcasecmp(run_direct_string, "ON")  == 0)
+     || (strcasecmp(run_direct_string, "YES") == 0)
+     || (strcasecmp(run_direct_string, "1")   == 0)) {
+        *run_direct = 1;
+        
+    } else if ((strcasecmp(run_direct_string, "OFF") == 0)
+            || (strcasecmp(run_direct_string, "NO")  == 0)
+            || (strcasecmp(run_direct_string, "0")   == 0)) {
+        *run_direct = 0;
+    } else {
+        if (rank == 0) {
+            printf("[random cube example] ERROR! Undefined run direct token \"%s\". Exiting.\n",
+                   run_direct_string);
+        }
+        exit(1);
+    }
+    
+    
+
+    RunParams_Setup(run_params,
+                    kernel, num_kernel_params, kernel_params,
+                    approximation, singularity, compute_type,
+                    theta, size_check_factor, interp_order, 
+                    max_per_source_leaf, max_per_target_leaf,
+                    verbosity);
+
+    return;
+}
+
+
+
+
 /*----------------------------------------------------------------------------*/
 double Point_Set_Init(DISTRIBUTION distribution)
 {
