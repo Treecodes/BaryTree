e66d89ff4bd2b640a0a40d4139d8e2ae275f8be9
Leighton W. Wilson <leightonwilson@mac.com>
2020-08-24 15:54:41 -0400

    removing header dependency

--- src/interface/BaryTreeInterface.h
+++ src/interface/BaryTreeInterface.h
@@ -1,8 +1,50 @@
 #ifndef H_BARYTREE_INTERFACE_H
 #define H_BARYTREE_INTERFACE_H
 
-#include "../utilities/enums.h"
-
+    #ifndef H_BARYTREE_TYPES_H
+    #define H_BARYTREE_TYPES_H
+    
+    typedef enum KERNEL
+    {
+        NO_KERNEL,
+        COULOMB,
+        YUKAWA,
+        REGULARIZED_COULOMB,
+        REGULARIZED_YUKAWA,
+        ATAN,
+        TCF,
+        DCF,
+        SIN_OVER_R,
+        MQ
+    } KERNEL;
+    
+    
+    typedef enum SINGULARITY
+    {
+        NO_SINGULARITY,
+        SKIPPING,
+        SUBTRACTION
+    } SINGULARITY;
+    
+    
+    typedef enum APPROXIMATION
+    {
+        NO_APPROX,
+        LAGRANGE,
+        HERMITE
+    } APPROXIMATION;
+    
+    
+    typedef enum COMPUTE_TYPE
+    {
+        NO_COMPUTE_TYPE,
+        PARTICLE_CLUSTER,
+        CLUSTER_PARTICLE,
+        CLUSTER_CLUSTER,
+    } COMPUTE_TYPE;
+    
+    
+    #endif /* H_BARYTREE_TYPES_H */
 
 void BaryTreeInterface(int numTargets, int numSources,
 		double *targetX, double *targetY, double *targetZ, double *targetValue,
