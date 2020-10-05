a1327d71be64f4d464aca0b0330add3da7804cb7
Nathan Vaughn <njvaughn@umich.edu>
2020-10-05 15:20:42 -0400

    Create + Zeroing fixes the issue.

--- src/drivers/treedriver.c
+++ src/drivers/treedriver.c
@@ -345,8 +345,21 @@ void treedriver(struct Particles *sources, struct Particles *targets, struct Run
         if (run_params->singularity == SUBTRACTION) {
             #pragma acc enter data copyin(targets->q[0:targets->num])
         }
-        #pragma acc enter data copyin(potential[0:targets->num])
+        #pragma acc enter data create(potential[0:targets->num])
 #endif
+
+
+#ifdef OPENACC_ENABLED
+    #pragma acc kernels present(potential)
+    {
+        for (int i=0;i<targets->num;i++){
+            potential[i]=0.0;
+        }
+    }
+#endif
+
+
+
         STOP_TIMER(&time_tree[1]);
         
         START_TIMER(&time_tree[2]);
