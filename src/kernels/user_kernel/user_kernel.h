3fabb95e3eed9845ccafa1a974422e4808d46dab
Nathan Vaughn <njvaughn@umich.edu>
2020-10-07 14:13:49 -0400

    Adding user_kernels directory

---
+++ src/kernels/user_kernel/user_kernel.h
@@ -0,0 +1,9 @@
+/* Interaction Kernels */
+#ifndef H_K_USER_KERNEL_H
+#define H_K_USER_KERNEL_H
+ 
+#include "user_kernel_pp.h"
+#include "user_kernel_pc.h"
+#include "user_kernel_cp.h"
+
+#endif /* H_K_USER_KERNEL_H */
